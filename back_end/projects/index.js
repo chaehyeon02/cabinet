1a64979c39df3078dc27aeb763cdd83e4fc84284
E_Jaeae <80445063+EunjaeJo@users.noreply.github.com>
2023-12-13 15:43:03 +0900

    [REFACTOR] Update URL API

--- back_end/projects/index.js
+++ back_end/projects/index.js
@@ -2,7 +2,8 @@ const express = require('./config/express');
 const { logger } = require('./config/winston');
 
 const port = 3000;
-const serverURL = 'http://34.64.116.143'; // 서버URL
+const endpointPath = '/app/1/notices'; // 공지글 조회 API
+const serverURL = `http://34.64.116.143${endpointPath}`;
 
 express().listen(port);
 
